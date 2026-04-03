fd6cc60e2cbeac1040de2aeae1b09d4632a86938
samir karki <samirkarki123@gmail.com>
2026-04-03 22:49:01 +0545

    Use data from the backend to generate the HTML

--- Ecommerce-Website/src/Pages/HomePage.jsx
+++ Ecommerce-Website/src/Pages/HomePage.jsx
@@ -1,16 +1,18 @@
-import axios from 'axios';
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { Header } from "../components/Header";
-import { products } from '../../Starting-Code/data/products'
+// import { products } from "../../Starting-Code/data/products";
 import "./HomePage.css";
 
 export function HomePage() {
-axios.get('http://localhost:3000/api/products')
-   .then((response) => {
-   console.log(response.data);
-   
-    
-   })
-  
+
+  const [products, setProducts] = useState([])
+  useEffect(() => {
+    axios.get("http://localhost:3000/api/products").then((response) => {
+      setProducts(response.data);
+    });
+  }, []);
+
   return (
     <>
       <title>Ecommerce Project</title>
@@ -19,56 +21,57 @@ axios.get('http://localhost:3000/api/products')
       <div className="home-page">
         <div className="products-grid">
           {products.map((product) => {
-             return (
-              <div key = {product.id} className="product-container">
-            <div className="product-image-container">
-              <img
-                className="product-image"
-                src= {product.image}
-              />
-            </div>
+            return (
+              <div key={product.id} className="product-container">
+                <div className="product-image-container">
+                  <img className="product-image" src={product.image} />
+                </div>
 
-            <div className="product-name limit-text-to-2-lines">
-              Black and Gray Athletic Cotton Socks - 6 Pairs
-            </div>
+                <div className="product-name limit-text-to-2-lines">
+                  Black and Gray Athletic Cotton Socks - 6 Pairs
+                </div>
 
-            <div className="product-rating-container">
-              <img
-                className="product-rating-stars"
-                src={`./images/ratings/rating-${product.rating.stars * 10}.png`}
-              />
-              <div className="product-rating-count link-primary">{product.rating.count}</div>
-            </div>
+                <div className="product-rating-container">
+                  <img
+                    className="product-rating-stars"
+                    src={`./images/ratings/rating-${product.rating.stars * 10}.png`}
+                  />
+                  <div className="product-rating-count link-primary">
+                    {product.rating.count}
+                  </div>
+                </div>
 
-            <div className="product-price">${(product.priceCents / 100).toFixed(2)}</div>
+                <div className="product-price">
+                  ${(product.priceCents / 100).toFixed(2)}
+                </div>
 
-            <div className="product-quantity-container">
-              <select>
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-                <option value="6">6</option>
-                <option value="7">7</option>
-                <option value="8">8</option>
-                <option value="9">9</option>
-                <option value="10">10</option>
-              </select>
-            </div>
+                <div className="product-quantity-container">
+                  <select>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                    <option value="6">6</option>
+                    <option value="7">7</option>
+                    <option value="8">8</option>
+                    <option value="9">9</option>
+                    <option value="10">10</option>
+                  </select>
+                </div>
 
-            <div className="product-spacer"></div>
+                <div className="product-spacer"></div>
 
-            <div className="added-to-cart">
-              <img src="./images/icons/checkmark.png" />
-              Added
-            </div>
+                <div className="added-to-cart">
+                  <img src="./images/icons/checkmark.png" />
+                  Added
+                </div>
 
-            <button className="add-to-cart-button button-primary">
-              Add to Cart
-            </button>
-          </div>
-             )
+                <button className="add-to-cart-button button-primary">
+                  Add to Cart
+                </button>
+              </div>
+            );
           })}
         </div>
       </div>
