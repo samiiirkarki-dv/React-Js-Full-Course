2c7aada633d56dee2b6ad9b792b9bbd2a283c08b
samir karki <samirkarki123@gmail.com>
2026-04-05 22:42:53 +0545

    Convert homepage to async await

--- Ecommerce-Website/src/Pages/Home/HomePage.jsx
+++ Ecommerce-Website/src/Pages/Home/HomePage.jsx
@@ -8,9 +8,12 @@ export function HomePage({ cart }) {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    axios.get("/api/products").then((response) => {
+    const getHomeData = async () => {
+      const response = await axios.get("/api/products");
       setProducts(response.data);
-    });
+    };
+
+    getHomeData();
   }, []);
 
   return (
@@ -19,7 +22,7 @@ export function HomePage({ cart }) {
       <Header cart={cart} />
 
       <div className="home-page">
-       <ProductsGrid products={products} />
+        <ProductsGrid products={products} />
       </div>
     </>
   );
