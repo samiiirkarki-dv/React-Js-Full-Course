4576df5722d0343e0cfa09010b018e904de7dc14
samir karki <samirkarki123@gmail.com>
2026-04-05 22:43:43 +0545

    Added async and await to homepage and checkout page

--- Ecommerce-Website/src/App.jsx
+++ Ecommerce-Website/src/App.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { Routes, Route } from "react-router";
-import { useState, useEffect  } from "react";
+import { useState, useEffect } from "react";
 import { HomePage } from "./Pages/Home/HomePage";
 import { CheckoutPage } from "./Pages/checkout/CheckoutPage";
 import { OrderPages } from "./Pages/orders/OrderPages";
@@ -11,14 +11,17 @@ function App() {
   const [cart, setCart] = useState([]);
 
   useEffect(() => {
-    axios.get("/api/cart-items?expand=products").then((response) => {
+    const fetchAppData = async () => {
+      const response = await axios.get("/api/cart-items?expand=products");
       setCart(response.data);
-    });
+    };
+
+    fetchAppData();
   }, []);
 
   return (
     <Routes>
-      <Route path="/" element={<HomePage  cart={cart} />} />
+      <Route path="/" element={<HomePage cart={cart} />} />
       <Route path="/checkout" element={<CheckoutPage cart={cart} />} />
       <Route path="/orders" element={<OrderPages cart={cart} />} />
       <Route path="/tracking" element={<TrackingPage />} />
