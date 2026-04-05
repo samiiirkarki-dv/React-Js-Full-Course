d8776001d8862ed24ccf11ca45006e1178d30a10
samir karki <samirkarki123@gmail.com>
2026-04-05 22:42:02 +0545

    Convert checkout page to async await

--- Ecommerce-Website/src/Pages/checkout/CheckoutPage.jsx
+++ Ecommerce-Website/src/Pages/checkout/CheckoutPage.jsx
@@ -9,15 +9,18 @@ export function CheckoutPage({ cart }) {
   const [payementSummary, setPaymentSummary] = useState(null);
 
   useEffect(() => {
-    axios
-      .get("/api/delivery-options?expand=estimatedDeliveryTime")
-      .then((response) => {
-        setDeliveryOptions(response.data);
-      });
+    const fetchCheckoutData = async () => {
+      let response = await axios.get(
+        "/api/delivery-options?expand=estimatedDeliveryTime",
+      );
 
-    axios.get("/api/payment-summary").then((response) => {
+      setDeliveryOptions(response.data);
+
+      response = await axios.get("/api/payment-summary");
       setPaymentSummary(response.data);
-    });
+    };
+
+    fetchCheckoutData();
   }, []);
 
   return (
@@ -52,7 +55,7 @@ export function CheckoutPage({ cart }) {
         <div className="checkout-grid">
           <OrderSummary cart={cart} deliveryOptions={deliveryOptions} />
 
-         <OrderSummary payementSummar={payementSummary} />
+          <OrderSummary payementSummar={payementSummary} />
         </div>
       </div>
     </>
