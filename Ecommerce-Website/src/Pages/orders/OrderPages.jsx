fc644b71ced28ad14768329b2b0d7484021ce446
samir karki <samirkarki123@gmail.com>
2026-04-05 22:19:34 +0545

    Seperate smaller components

--- Ecommerce-Website/src/Pages/orders/OrderPages.jsx
+++ Ecommerce-Website/src/Pages/orders/OrderPages.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 import dayjs from "dayjs";
 import { useEffect, useState, Fragment } from "react";
-import { Header } from "../components/Header";
-import { formatMoney } from "../utils/money";
+import { Header } from "../../components/Header";
+import { formatMoney } from "../../utils/money";
 import "./OrderPages.css";
 
 export function OrderPages({ cart }) {
