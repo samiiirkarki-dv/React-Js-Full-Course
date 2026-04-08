b655aee02ec5cd1937e7ef92fd4271b53f176afc
samir karki <samirkarki123@gmail.com>
2026-04-08 20:32:15 +0545

    Create integration test for Product component

--- Ecommerce-project/src/pages/home/product.jsx
+++ Ecommerce-project/src/pages/home/product.jsx
@@ -22,7 +22,9 @@ const selectQuantity = (event) => {
     return (
           <div  className="product-container">
             <div className="product-image-container">
-              <img className="product-image" src={product.image} />
+              <img className="product-image"
+              data-testid = "product-image"
+              src={product.image} />
             </div>
 
             <div className="product-name limit-text-to-2-lines">
@@ -32,6 +34,7 @@ const selectQuantity = (event) => {
             <div className="product-rating-container">
               <img
                 className="product-rating-stars"
+                data-testid="product-rating-stars-image"
                 src={`images/ratings/rating-${product.rating.stars * 10}.png`}
               />
               <div className="product-rating-count link-primary">
