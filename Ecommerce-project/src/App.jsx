9742b48842e134eebee6e24ecf1246733cf66278
samir karki <samirkarki123@gmail.com>
2026-04-08 16:06:51 +0545

    Create update delivery option feature

--- Ecommerce-project/src/App.jsx
+++ Ecommerce-project/src/App.jsx
@@ -23,7 +23,7 @@ const loadCart = async () => {
   return (
     <Routes>
       <Route index element={<HomePage cart={cart} loadCart={loadCart} />} />
-      <Route path="checkout" element={<CheckoutPage cart={cart} />} />
+      <Route path="checkout" element={<CheckoutPage cart={cart} loadCart={loadCart} />} />
       <Route path="orders" element={<OrdersPage cart={cart} />} />
     </Routes>
   )
