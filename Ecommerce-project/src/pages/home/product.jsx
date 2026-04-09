852bea5daaf969a9ada70c655c34903e5f14e79c
samir karki <samirkarki123@gmail.com>
2026-04-09 17:06:38 +0545

    Test clicking add to cart button

--- Ecommerce-project/src/pages/home/product.jsx
+++ Ecommerce-project/src/pages/home/product.jsx
@@ -70,6 +70,7 @@ const selectQuantity = (event) => {
 
             <button
               className="add-to-cart-button button-primary"
+              data-testid="add-to-cart-button"
               onClick={addToCart}
             >
               Add to Cart
